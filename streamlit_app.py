5625261c8e76d169b32d8e8a96bda7224b6e52a0
David Tomlins <36961142+dreadeyedave@users.noreply.github.com>
2022-11-15 18:31:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
+#MENU
 streamlit.title('My Parents New Healthy Diner')
-
+#Breakfast
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -24,10 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#Fruity fruit advice 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:       
+    streamlit.write('The user entered ', fruit_choice)
+
+except URLError as a:
+    streamlit.error()
+    
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 # Normalises the JSON response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output json as table
